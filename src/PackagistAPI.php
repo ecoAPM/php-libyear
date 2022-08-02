3923f5e4a67efb449594b41993751500c598f424
Prajapati Kaushik <prajapatikaushik@gmail.com>
2022-08-02 11:54:57 +0100

    fix: php parse error

--- src/PackagistAPI.php
+++ src/PackagistAPI.php
@@ -7,30 +7,30 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class PackagistAPI
 {
-	private ClientInterface $http_client;
+    private ClientInterface $http_client;
 
-	/** @var resource */
-	private $stderr;
+    /** @var resource */
+    private $stderr;
 
-	/**
-	 * @param ClientInterface $http_client
-	 * @param resource $stderr
-	 */
-	public function __construct(ClientInterface $http_client, $stderr)
-	{
-		$this->http_client = $http_client;
-		$this->stderr = $stderr;
-	}
+    /**
+     * @param resource $stderr
+     */
+    public function __construct(ClientInterface $http_client, $stderr)
+    {
+        $this->http_client = $http_client;
+        $this->stderr = $stderr;
+    }
 
-	public function getPackageInfo(string $package): array
-	{
-		try {
-			$response = $this->http_client->request('GET', "https://repo.packagist.org/packages/{$package}.json");
-			return json_decode($response->getBody()->getContents(), true) ?? [];
+    public function getPackageInfo(string $package): array
+    {
+        try {
+            $response = $this->http_client->request('GET', "https://repo.packagist.org/packages/{$package}.json");
 
-		} catch (GuzzleException) {
-			fwrite($this->stderr, "Could not find info for {$package} on Packagist\n");
-			return [];
-		}
-	}
+            return json_decode($response->getBody()->getContents(), true) ?? [];
+        } catch (GuzzleException $e) {
+            fwrite($this->stderr, "Could not find info for {$package} on Packagist\n");
+
+            return [];
+        }
+    }
 }
