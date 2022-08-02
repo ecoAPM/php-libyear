5ac26b2ddb972744a2c6a3ecbcd11d90b7498b48
Kaushik Prajapati <10140681+prajapati-kaushik@users.noreply.github.com>
2022-08-02 17:21:46 +0530

    Update PackagistAPI.php

--- src/PackagistAPI.php
+++ src/PackagistAPI.php
@@ -27,7 +27,7 @@ class PackagistAPI
 		try {
 			$response = $this->http_client->request('GET', "https://repo.packagist.org/packages/{$package}.json");
 			return json_decode($response->getBody()->getContents(), true) ?? [];
-        } catch (GuzzleException $e) {
+		} catch (GuzzleException $e) {
 			fwrite($this->stderr, "Could not find info for {$package} on Packagist\n");
 			return [];
 		}
