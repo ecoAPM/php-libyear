25e3c9576c4dc2d85d1f9e28d751924ce4fc73d5
Steve Desmond <Steve@ecoAPM.com>
2022-09-28 09:32:35 -0400

    Fix null string replacement deprecation

--- src/App.php
+++ src/App.php
@@ -45,10 +45,10 @@ class App
 			array_map(fn(Dependency $dependency): array => [
 				$dependency->name,
 				$dependency->current_version->version_number,
-				isset($dependency->current_version->released) ? $dependency->current_version->released->format('Y-m-d') : null,
-				isset($dependency->newest_version->version_number) ? $dependency->newest_version->version_number : null,
-				isset($dependency->newest_version->released) ? $dependency->newest_version->released->format('Y-m-d') : null,
-				$dependency->getLibyearsBehind() !== null ? number_format($dependency->getLibyearsBehind(), 2) : null
+				isset($dependency->current_version->released) ? $dependency->current_version->released->format('Y-m-d') : "",
+				isset($dependency->newest_version->version_number) ? $dependency->newest_version->version_number : "",
+				isset($dependency->newest_version->released) ? $dependency->newest_version->released->format('Y-m-d') : "",
+				$dependency->getLibyearsBehind() !== null ? number_format($dependency->getLibyearsBehind(), 2) : ""
 			], $dependencies)
 		);
 
